e817ff8b33d1b959247e156b171460aaec09c9c3
Ian Morrison <ian@yola.com>
2014-04-08 12:16:06 -0700

    Make the testing names more consistent

--- tests/middleware_tests.py
+++ tests/middleware_tests.py
@@ -15,17 +15,19 @@ class CheckBrowserMiddlewareTest(SimpleTestCase):
         self.response = Mock()
         self.cbmw = UnsupportedBrowsersMiddleware()
 
-    def test_redirect_unsupported_browser(self):
+        self.IE8 = 'Mozilla/5.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0; GTB7.4; InfoPath.2; SV1; .NET CLR 3.3.69573; WOW64; en-US)'
+
+    def test_redirects_an_unsupported_browser(self):
         self.request.META['HTTP_USER_AGENT'] = 'Mozilla/5.0 (Windows; U; MSIE 7.0; Windows NT 6.0; en-US)'
         response = self.cbmw.process_response(self.request, self.response)
         self.assertIn(settings.UNSUPPORTED_URL, response.url)
 
-    def test_redirects_IE_8_to_unsupported_browser(self):
-        self.request.META['HTTP_USER_AGENT'] = 'Mozilla/5.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0; GTB7.4; InfoPath.2; SV1; .NET CLR 3.3.69573; WOW64; en-US)'
+    def test_redirects_IE_8_to_unsupported_browser_page(self):
+        self.request.META['HTTP_USER_AGENT'] = self.IE8
         response = self.cbmw.process_response(self.request, self.response)
         self.assertIn('unsupported-browser', response.url)
 
-    def test_supported_browsers_are_not_redirected(self):
+    def test_does_not_redirect_supported_browsers(self):
         ie11 = 'Mozilla/4.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/5.0)'
         self.request.META['HTTP_USER_AGENT'] = ie11
         response = self.cbmw.process_response(self.request, self.response)
